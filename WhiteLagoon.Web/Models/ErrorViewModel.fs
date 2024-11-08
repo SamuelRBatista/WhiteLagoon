9a9a79474561eec77d2ef370928f606716781457
SamuelRBatista <samuel.r.batista@outlook.com>
2024-11-08 10:00:38 -0300

    first commit

---
+++ WhiteLagoon.Web/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace WhiteLagoon.Web.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
